dbedd75d9f2977d08d9f037f641f937e963daebd
mosa-150505 <mosa.raharimanou@gmail.com>
2026-04-16 17:00:47 +0300

    add global functions for the algo

--- src/main.c
+++ src/main.c
@@ -42,7 +42,7 @@ typedef struct {
     int num_optimal_points;
 } Solution;
 
-//  Functions
+//  Internal Functions
 static bool is_constraint_satisfied (const Constraint *c, double x, double y) {
     return (c->a * x + c->b * y) <= (c->d + EPSILON); //  EPSILON sert à la tolérance des valeurs, car les double sont souvent imprécis
 }
@@ -70,4 +70,62 @@ static bool calculate_intersection (const Line *d1, const Line *d2, Point *out) 
     out->y = (d1->a  * d2->d - d2->a * d1->d) / det;
 
     return true;
+}
+
+static double calculate_z (double c1, double c2, double x, double y) {
+    return c1*x + c2*y;
+}
+
+static bool are_z_equal (double z1, double z2) {
+    return fabs(z1 - z2) < EPSILON;
+}
+
+static double distance (Point p1, Point p2) {
+    return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+}
+
+//  Global Functions
+void init_linear_program (LinearProgram *lp, double c1, double c2, OptimizationType type) {
+    lp->c1 = c1;
+    lp->c2 = c2;
+    lp->type = type;
+    lp->num_constraints = 0;
+    lp->num_lines = 0;
+}
+
+void add_constraint (LinearProgram *lp, double a, double b, double d) {
+    if (lp->num_constraints >= MAX_CONSTRAINTS) {
+        printf("\nNombre limite de contraintes atteint (max %d)\n", MAX_CONSTRAINTS);
+        return;
+    }
+
+    lp->constraints[lp->num_constraints].a = a;
+    lp->constraints[lp->num_constraints].b = b;
+    lp->constraints[lp->num_constraints].d = d;
+
+    lp->num_constraints++;
+}
+
+void build_lines (LinearProgram *lp) {
+    int i;
+
+    for (i = 0; i < lp->num_constraints; i++) {
+        lp->lines[i].a = lp->constraints[i].a;
+        lp->lines[i].b = lp->constraints[i].b;
+        lp->lines[i].d = lp->constraints[i].d;
+    }
+
+    lp->num_lines = lp->num_constraints;
+
+    // Axe des abscisses : x >= 0 : x = 0 (contrainte obli)
+    lp->lines[lp->num_lines].a = 1.0;
+    lp->lines[lp->num_lines].b = 0.0;
+    lp->lines[lp->num_lines].d = 0.0;
+    lp->num_lines++;
+
+    // Axe des ordonnees : y >= 0 : y = 0 (contrainte obli)
+    lp->lines[lp->num_lines].a = 0.0;
+    lp->lines[lp->num_lines].b = 1.0;
+    lp->lines[lp->num_lines].d = 0.0;
+    lp->num_lines++;
 }
