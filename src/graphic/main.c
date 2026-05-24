4cb590535a4f875ed8900750359b3f6a21e3b31f
mosa-150505 <mosa.raharimanou@gmail.com>
2026-05-24 18:52:19 +0300

    feat graphic: chech unbournitude

--- src/graphic/main.c
+++ src/graphic/main.c
@@ -135,3 +135,77 @@ int trouver_sommet_opt (int nb_droite, Droite droites[], double c1, double c2, d
 
     return nb_sommet_opt;
 }
+
+bool est_infini (int nb_contrainte, Droite droites[], double c1, double c2, double signe, double z_opt, Point sommet_opt) {
+    int i;
+    double intervalle, new_z;
+    Point new_pt;
+
+    intervalle = 1e6;
+
+    double directions[3][2] = {
+        {1.0, 0.0},
+        {0.0, 1.0},
+        {c1 * signe, c2 * signe}
+    };
+
+    for (i = 0; i < 3; i++) {
+        new_pt = (Point){
+            sommet_opt.x + directions[i][0] * intervalle,
+            sommet_opt.y + directions[i][1] * intervalle,
+        };
+
+        if (est_valide(nb_contrainte, droites, new_pt)) {
+            new_z = (c1 * new_pt.x + c2 * new_pt.y) * signe;
+
+            if (new_z > z_opt + EPSILON)
+                return true;
+        }
+    }
+
+    return false;
+}
+
+void afficher_resultats (int nb_sommet_opt, Point sommets_opt[], double z_opt, double signe) {
+    printf("\n-- RESULTAT --\n");
+
+    if (nb_sommet_opt == 0) {
+        printf("\n*Pas de solution\n");
+        return;
+    }
+
+    if (nb_sommet_opt > 1) {
+        printf("\n*%s atteint entre (%.2f, %.2f) et (%.2f, %.2f)\n",
+                signe == 1.0 ? "Max" : "Min", 
+                sommets_opt[0].x, sommets_opt[0].y,
+                sommets_opt[nb_sommet_opt - 1].x, sommets_opt[nb_sommet_opt - 1].y
+        );
+    }
+    
+    else {
+        printf("\n*%s atteint en (%.4f, %.4f)\n", signe == 1.0 ? "Max" : "Min", sommets_opt[0].x, sommets_opt[0].y);
+    }
+
+    printf("\n\tz%s = %.4f\n", signe == 1.0 ? "max" : "min", z_opt);
+}
+
+int main () {
+    Droite droites[105];
+    Point sommets_opt[100];
+    int nb_droite, nb_contrainte, nb_sommet_opt;
+    double c1, c2, signe, z_opt;
+
+    nb_droite = saisir_donnees(&signe, &c1, &c2, droites);
+    
+    nb_sommet_opt = trouver_sommet_opt(nb_droite, droites, c1, c2, signe, &z_opt, sommets_opt);
+
+    nb_contrainte = nb_droite - 2;
+
+    if (nb_sommet_opt > 0 && est_infini(nb_contrainte, droites, c1, c2, signe, z_opt, sommets_opt[0]))
+        printf("\n*Ensemble des solutions possibles non borne : z%s = inf\n", signe == 1.0 ? "max" : "min");
+
+    else 
+        afficher_resultats(nb_sommet_opt, sommets_opt, z_opt, signe);
+
+    return 0;
+}
